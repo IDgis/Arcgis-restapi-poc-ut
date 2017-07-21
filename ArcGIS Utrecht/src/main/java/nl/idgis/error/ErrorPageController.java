b0070730dc1e5f7a996c299b03e3dd079dfe0024
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-07-21 12:01:56 +0200

    Changed class name to show it's a Controller

--- ArcGIS Utrecht/src/main/java/nl/idgis/error/ErrorPageController.java
+++ ArcGIS Utrecht/src/main/java/nl/idgis/error/ErrorPageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ErrorPageHandler implements ErrorController {
+public class ErrorPageController implements ErrorController {
 
 	private static final String PATH = "/error";
 	

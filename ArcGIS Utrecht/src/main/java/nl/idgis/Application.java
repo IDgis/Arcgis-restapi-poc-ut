06b0ba343632113280072ac14dfec2a569b0d9fe
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-08-01 10:55:10 +0200

    Removed unused constructor

--- ArcGIS Utrecht/src/main/java/nl/idgis/Application.java
+++ ArcGIS Utrecht/src/main/java/nl/idgis/Application.java
@@ -7,10 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages={"nl.idgis", "nl.idgis.query"})
 public class Application {
-	
-	public Application() {
-		// Empty constructor
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
